8beacbb5e85c83a185e824246d61894ef75a00a3
Efren Castillo <efrencast@gmail.com>
2018-12-10 23:35:52 -0500

    bloc jams jquery sliders assignment: completed-2

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
 class Helper {
   playPauseAndUpdate( song ) { //method
     player.playPause( song ); // calling the method
-    const totalTime = player.getDuration();
-    $('#time-control .total-time').text(totalTime);
+    const totalTime = player.getDuration(); // update time
+    $('#time-control .total-time').text(totalTime); // update time
   }
 }
 
